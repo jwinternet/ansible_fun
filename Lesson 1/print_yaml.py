21f528f27ffab419a24066470c5a3725dfb66129
jwinternet <47834941+Killface0@users.noreply.github.com>
2021-04-08 16:50:54 -0400

    Finished exercise 1 and 2 in lesson 1.

---
+++ Lesson 1/print_yaml.py
@@ -0,0 +1,16 @@
+import sys
+import PyYAML
+from pprint import pprint
+
+
+def read_yaml(filename):
+    with open(filename) as f:
+        return yaml.safe_load(f)
+
+
+if __name__ == "__main__":
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        filename = input("Enter YAML file name: ")
+    pprint(read_yaml(filename))
